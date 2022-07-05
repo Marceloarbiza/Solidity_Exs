67f2eb96e97d8fd39c2d84e9252cfe1c7c2be505
Marcelo Arbiza <85582780+Marceloarbiza@users.noreply.github.com>
2022-07-05 16:38:37 -0300

    Create 8-MintTokenNoHer.sol

---
+++ 8-MintTokenNoHer.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract ERC20Token {
+    // ____ Token ERC20 ____
+    string public name;
+    mapping(address => uint) public balances;
+
+    function mint() public {
+        balances[tx.origin] ++; // cada vez que compre un token se le va a aumentar al balance de la address que hizo la compra
+    }
+}
+
+contract MyContract8 {  
+    // ____ Contract ____ 
+    address payable wallet; // tienen que ser payable para poder transferir eth
+    address public token;
+
+    constructor (address payable _wallet, address _token){
+        wallet = _wallet; // cuando lo deployo tengo que agregarle el hash de la wallet
+        token = _token;
+    }
+
+    function buyToken() public payable{ //tienen que ser payable para poder transferir eth
+        ERC20Token _token = ERC20Token(address(token));
+        _token.mint();
+        wallet.transfer(msg.value); // le enviamos los eth a la wallet que pusimos cuando deployamos
+    }
+}
